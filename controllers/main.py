4b35a6404d15f9da1f21a60f2d8fc43b90bdc7d6
Hagliberto <hagliberto@gmail.com>
2025-02-09 21:42:46 -0300

    Melhorias na U.I.

--- controllers/main.py
+++ controllers/main.py
@@ -118,51 +118,78 @@ def main():
         st.subheader(":blue[**Perguntas**]")
         with st.form(key='resposta_form'):
             st.success(f':blue[**Exercício**] :gray[**({st.session_state.operacao}):**] :red[**{st.session_state.pergunta}**]')
-        
-            col1, col2, col3 = st.columns([1, 3, 1])
-            
+            col1, col2, col3 = st.columns([1,3,1])
+           
             with col2:
-                resposta_usuario = st.text_input(":green[**Digite sua resposta⤵️**]", 
-                                                 key="campo_resposta", 
-                                                 placeholder="✍🏻 Insira sua resposta aqui")
-        
-            col4, col5 = st.columns(2)
-            with col4:
-                responder = st.form_submit_button(":green[**Responder**] **Pergunta**", 
-                                                  help="Digite sua resposta e clique em 'Responder' para verificar se está correta.", 
-                                                  key="botao_responder")
-            with col5:
-                pular = st.form_submit_button(":green[**Pular**] **Pergunta**", 
-                                              help="Pula a pergunta atual e exibe uma nova pergunta")
-        
+                if "campo_resposta" not in st.session_state:
+                    st.session_state["campo_resposta"] = ""
+    
+
+                resposta_usuario = st.text_input(":green[**Digite sua resposta⤵️**]", key="campo_resposta", placeholder="✍🏻 Insira sua resposta aqui")
+                
+                # Converter vírgula para ponto e transformar em número
+                try:
+                    resposta_numerica = float(resposta_usuario.replace(',', '.'))
+                except ValueError:
+                    st.write("Por favor, insira um número válido.")
+                    
+                
+
+
+
+                st.caption("Digite sua resposta e clique em 'Responder' para verificar se está correta.")
+                # exibir_pontuacao()
+
+
+            with col2:
+                col4, col5 = st.columns(2) 
+                with col4:               
+                    responder = st.form_submit_button(":green[**Responder**] **Pergunta**", help="Digite sua resposta e clique em 'Responder' para verificar se está correta.", icon=":material/add_task:")
+                with col5:    
+                    pular = st.form_submit_button(":green[**Pular**] **Pergunta**", help="Pula a pergunta atual e exibe uma nova pergunta", icon=":material/move_down:")
+
+            with col3:
+                st.image("assets/cat-cats.gif", use_container_width=True, width=100, caption=" ")
+
+
             if responder:
                 try:
-                    if resposta_usuario.strip():
-                        resposta_numerica = float(resposta_usuario.replace(',', '.'))
-                        resultado = "Certo" if resposta_numerica == st.session_state.resposta_correta else "Errado"
+                    resposta_usuario = float(resposta_usuario.strip()) if resposta_usuario.strip() else None
+
+                    if resposta_usuario is not None:
+                        resultado = "Certo" if resposta_usuario == st.session_state.resposta_correta else "Errado"
                         st.session_state.pontos[resultado] += 1
-        
+
                         data_atual = datetime.datetime.now().date()
                         ultima_resposta = [[st.session_state.nome, data_atual, st.session_state.operacao, resultado]]
                         salvar_pontuacao_por_dia(st.session_state.nome, st.session_state.pontos, data_atual, ultima_resposta)
-        
+
+                        st.session_state.pop("campo_resposta", None)
+
                         st.session_state.pergunta, st.session_state.resposta_correta, st.session_state.operacao = gerar_pergunta(operacoes_selecionadas, dificuldade.lower())
+
                         st.rerun()
                     else:
-                        st.error("Por favor, insira um número válido.")
+                        with col2:
+                            st.error("Por favor, insira um número válido.")
+                            st.toast("Por favor, insira um número válido.", icon=":material/pin:")
                 except ValueError:
-                    st.error("Por favor, insira um número válido.")
-        
+                    with col2:                    
+                        st.error("Por favor, insira um número válido.")
+                        st.toast("Por favor, insira um número válido.", icon=":material/123:")
+
+
             if pular:
                 st.session_state.pontos["Pulado"] += 1
+
                 data_atual = datetime.datetime.now().date()
                 st.session_state.historico.append([st.session_state.nome, data_atual, st.session_state.operacao, "Pulado"])
-        
+
                 st.session_state.pergunta, st.session_state.resposta_correta, st.session_state.operacao = gerar_pergunta(operacoes_selecionadas, dificuldade.lower())
-        
+
                 salvar_pontuacao_por_dia(st.session_state.nome, st.session_state.pontos, data_atual, st.session_state.historico)
+
                 st.rerun()
-        
 
     st.subheader(" ", divider="rainbow")
     exibir_pontuacao()
